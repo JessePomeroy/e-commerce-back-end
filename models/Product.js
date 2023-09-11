936f82bbab33b46c5e29675b674d3d8f350a01bf
JessePomeroy <thinkingofview@gmail.com>
2023-09-11 16:18:15 -0500

    models product tweak

--- models/Product.js
+++ models/Product.js
@@ -4,28 +4,37 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
+    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id',
-      },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    tag_id: {
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: true,
+    },
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'tag',
+        model: 'category',
         key: 'id',
       },
     },
